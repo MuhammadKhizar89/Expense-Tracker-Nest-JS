f6d541ffb0dc28153ff758cd30acc0e78600a2d6
Muhammad Khizar <muhammadkhizar8919@gmail.com>
2025-07-24 22:41:21 +0500

    / route

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  
+export class AppService {  
   getHello(): string {
-    return 'Hello World!';
+    return 'Want to use it ? Open Documentation ->https://docs.google.com/document/d/1G2tg1dA_XINfvfN1Inco4hIFYkwFRhuCmJv2i4iinP4/edit?tab=t.0';
   }
 }
